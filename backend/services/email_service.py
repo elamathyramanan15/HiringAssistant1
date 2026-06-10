7af6279c7871697b3c68e50eaa6011ad18712413
SanjayKK10 <kksanjay2410@gmail.com>
2026-06-10 14:21:17 +0530

    Fix forgot password options request

--- backend/services/email_service.py
+++ backend/services/email_service.py
@@ -1,31 +1,55 @@
 from flask_mail import Message
+
 from extensions import mail
-
-
+ 
+ 
 def send_reset_password_email(to_email, reset_link):
-    subject = "Reset Your HireAI Password"
 
-    body = f"""
+    try:
+
+        subject = "Reset Your HireAI Password"
+ 
+        body = f"""
+
 Hello,
-
+ 
 We received a request to reset your HireAI account password.
-
+ 
 Please click the link below to create a new password:
-
+ 
 {reset_link}
-
+ 
 This link will expire in 30 minutes.
-
+ 
 If you did not request this, please ignore this email.
-
+ 
 Regards,
+
 HireAI Team
+
 """
+ 
+        msg = Message(
 
-    msg = Message(
-        subject=subject,
-        recipients=[to_email],
-        body=body
-    )
+            subject=subject,
 
-    mail.send(msg)
+            recipients=[to_email],
+
+            body=body
+
+        )
+ 
+        print(f"Sending reset password email to: {to_email}")
+
+        mail.send(msg)
+
+        print("Reset password email sent successfully")
+ 
+        return True
+ 
+    except Exception as e:
+
+        print(f"EMAIL ERROR: {str(e)}")
+
+        raise
+ 
